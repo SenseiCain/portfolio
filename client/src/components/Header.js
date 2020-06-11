f9504f48355147b3443e3e0a6732b966ae230b7e
Christian Cain <christian22cain@yahoo.com>
2020-06-11 14:29:39 -0500

    styled header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-import PersonSVG from '../images/person-at-computer.svg';
-import LampSVG from '../images/lamp.svg';
-import SkateboardSVG from '../images/skateboard.svg';
+import { ReactComponent as PersonSVG } from '../images/person-at-computer-v2.svg';
+import { ReactComponent as LampSVG} from '../images/lamp.svg';
+import { ReactComponent as SkateboardSVG } from '../images/skateboard.svg';
 
-class Header extends Component {
-
-    render() {
-        return (
-            <div>
-                <h1>Header</h1>
+const Header = () => {
+    return (
+        <div className="wrapper">
+            <div className="header-container">
+                <SkateboardSVG className="svg-skateboard-1" />
+                <SkateboardSVG className="svg-skateboard-2" />
+                <PersonSVG className="svg-person" />
+                <LampSVG className="svg-lamp" />
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Header;
