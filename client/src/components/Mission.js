42087ec002c33af0e4d3326443075fab940475c7
Christian Cain <christian22cain@yahoo.com>
2020-06-11 13:31:06 -0500

    styled Mission & added ipsum

--- client/src/components/Mission.js
+++ client/src/components/Mission.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Mission extends Component {
-    render() {
-        return (
-            <h3>Mission</h3>
-        )
-    }
+const Mission = () => {
+    return (
+        <div className="wrapper">
+            <div className="mission-container">
+                <p>
+                    Davy Jones' Locker Spanish Main bounty chase maroon list Gold Road cackle fruit square-rigged trysail. 
+                    Barkadeer transom blow the man down gun lugsail line rutters gally gunwalls list. 
+                    Tack ballast run a rig gunwalls boatswain scuttle Sink me chase snow ho.
+                </p>
+            </div>
+        </div>
+    )
 }
 
 export default Mission;
