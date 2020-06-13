e5c744bf1d26d6cd1c135085ec7c495bcf05cc72
Christian Cain <christian22cain@yahoo.com>
2020-06-13 15:50:25 -0500

    created contact form

--- client/src/containers/ContactContainer.js
+++ client/src/containers/ContactContainer.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
 
 class ContactContainer extends Component {
+    constructor() {
+        super();
+        this.state = {
+            address: '',
+            subject: '',
+            message: ''
+        }
+    }
+
+    handleAddressChange = event => {
+        this.setState({ ...this.state, address: event.target.value })
+    }
+
+    handleSubjectChange = event => {
+        this.setState({ ...this.state, subject: event.target.value })
+    }
+
+    handleMessageChange = event => {
+        this.setState({ ...this.state, message: event.target.value })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        console.log(this.state)
+    }
+
     render() {
         return (
             <div className="wrapper">
                 <div className="contact-container">
-                    <h1>Contact</h1>
+                    <form onSubmit={this.handleSubmit}>
+                        <input 
+                            type="text" 
+                            value={this.state.address}
+                            placeholder="Email address"
+                            onChange={this.handleAddressChange}/>
+                        <input 
+                            type="text" 
+                            value={this.state.subject}
+                            placeholder="Subject"
+                            onChange={this.handleSubjectChange}/>
+                        <textarea 
+                            value={this.state.message}
+                            placeholder="Message..."
+                            rows="10"
+                            onChange={this.handleMessageChange}/>
+                        <input type="submit" />
+                    </form>
                 </div>
             </div>
         )
