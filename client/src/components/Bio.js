0b8f8e5db57bc3f8264c410edabb3af759b7fc22
Christian Cain <christian22cain@yahoo.com>
2020-06-13 12:32:07 -0500

    sketched bio section

--- client/src/components/Bio.js
+++ client/src/components/Bio.js
@@ -4,12 +4,36 @@ class Bio extends Component {
     render() {
         return (
             <div className="wrapper">
-                <div className="mission-container">
-                    <p>
+                <div id="bio-container">
+                    <div id="bio-left">
+                        <h1>Christian Cain</h1>
+                        <p>
                         Davy Jones' Locker Spanish Main bounty chase maroon list Gold Road cackle fruit square-rigged trysail. 
-                        Barkadeer transom blow the man down gun lugsail line rutters gally gunwalls list. 
-                        Tack ballast run a rig gunwalls boatswain scuttle Sink me chase snow ho.
-                    </p>
+                        Barkadeer transom blow the man down gun lugsail line rutters gally gunwalls list.
+                        </p>
+                        <ul>
+                            <li>
+                                <a href="" target="_blank">
+                                    GitHub
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" target="_blank">
+                                    LinkedIn
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" target="_blank">
+                                    Resume
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div id="bio-right">
+                        <div id="bio-image">
+                            
+                        </div>
+                    </div>
                 </div>
             </div>
         )
