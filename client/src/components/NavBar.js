f129a337c36994c2d85a28344b89f242daea08c4
Christian Cain <christian22cain@yahoo.com>
2020-06-14 11:45:15 -0500

    sticky NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
-const NavBar = () => {
-    return (
-        <div className="nav-container">
-            <nav className="wrapper">
-                <ul>
-                    <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="Blog">Blog</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/Contact">Contact</NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    )
+class NavBar extends Component {
+    componentDidMount() {
+        const navbar = document.getElementById("nav-container");
+        const sticky = navbar.offsetTop;
+
+        window.onscroll = () => {
+            if (window.pageYOffset >= sticky) {
+                navbar.classList.add("sticky")
+            } else {
+                navbar.classList.remove("sticky");
+            }
+        };
+    }
+
+    render() {
+        return (
+            <div id="nav-container" className="nav-container">
+                <nav className="wrapper">
+                    <ul>
+                        <li>
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="Blog">Blog</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/Contact">Contact</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        )
+    } 
 }
 
 export default NavBar;
