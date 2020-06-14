be22a9b62894cb3cef916611ab00c2fa5cedc08a
Christian Cain <christian22cain@yahoo.com>
2020-06-14 12:05:42 -0500

    sketched out blogs

---
+++ client/src/components/BlogLink.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BlogLink = props => {
+    return <li>{props.blog.title}</li>
+}
+
+export default BlogLink;
--- client/src/containers/BlogContainer.js
+++ client/src/containers/BlogContainer.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 import Footer from '../components/Footer';
+import BlogLink from '../components/BlogLink';
 
 class BlogContainer extends Component {
+    constructor() {
+        super();
+        this.state = {
+            titles: [
+                "MVC Overview",
+                "HearthStone Deck Builder application",
+                "Clickyz application",
+                "My Experience at Flatiron",
+                "Some other blog I have yet to write"
+            ]
+        }
+    }
+
+    generateBlogItems = () => {
+        return this.state.titles.map((t, i) => {
+            return <BlogLink key={i} blog={{title: t}} />
+        })
+    }
+    
     render() {
         return (
             <div className="wrapper">
                 <div className="blog-container">
-                    <h1>Blog</h1>
+                    <h1>Blogs</h1>
+                    <ul>
+                        {this.generateBlogItems()}
+                    </ul>
                 </div>
                 <Footer />
             </div>
