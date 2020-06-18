8f9bf16600381c50845c6f086293164000fa8f96
Christian Cain <christian22cain@yahoo.com>
2020-06-18 17:18:30 -0500

    added actual project data

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -1,7 +1,7 @@
 clickyz = Project.create(
     title: "Clickyz",
-    description: "Interactive mechanical keyboard editor. Made using a custom 3D keyboard model and p5.js for interactivity.",
-    img: "",
+    description: "Interactive mechanical keyboard editor. This was made using a custom 3D keyboard model and p5.js for interactivity. A personal project displaying my appreciation for hand made things.",
+    img: "https://raw.githubusercontent.com/SenseiCain/clickyz/master/preview.png",
     github: "https://github.com/SenseiCain/clickyz",
     heroku: ""
 )
@@ -10,7 +10,7 @@ clickyz.save
 
 hearthstone = Project.create(
     title: "HearthStone Deck Builder",
-    description: "SPA deck building application referencing a personal API, which is sourcing data from a community API.",
+    description: "SPA deck builder that dynamically displays card data & grants users the ability to build decks. This utilizes a standalone API, and also sources data in from a 3P API.",
     img: "https://raw.githubusercontent.com/SenseiCain/hearthstone-deck-builder/master/screenshot.png",
     github: "https://github.com/SenseiCain/hearthstone-deck-builder",
     heroku: ""
@@ -20,8 +20,8 @@ hearthstone.save
 
 sidetrack = Project.create(
     title: "Sidetrack",
-    description: "Server side rendered Reddit clone. This was a Flatiron project demonstrating key Rails fundamentals.",
-    img: "",
+    description: "Server-side rendered Reddit clone. This was a Flatiron project demonstrating key Rails fundamentals.",
+    img: "https://raw.githubusercontent.com/SenseiCain/sidetrack/master/preview.png",
     github: "https://github.com/SenseiCain/sidetrack",
     heroku: ""
 )
