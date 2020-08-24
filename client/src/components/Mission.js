47cd520f8dcb66a5dc04d070d999835e244ef3d8
Christian Cain <christian22cain@yahoo.com>
2020-08-24 14:27:02 -0500

    updated mission statement

--- client/src/components/Mission.js
+++ client/src/components/Mission.js
@@ -6,8 +6,7 @@ const Mission = () => {
             <p>
                 Creative young professional with a knack for tinkering and solving problems.
                 I am a recent <span>Flatiron School</span> grad with a strong focus in both <span>front end</span> & <span>back end</span> development.
-                I'm coming from a background in cyber security,
-                and am currently looking to make my first step towards a career in software engineering.
+                I'm currently looking to make my first step towards an exciting career in software engineering.
             </p>
         </div>
     )
