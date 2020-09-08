a8736d0fe672bf44db92af3af51354b4b4653eae
Christian Cain <christian22cain@yahoo.com>
2020-09-08 15:58:46 -0500

    project data stored locally

--- client/src/containers/ProjectContainer.js
+++ client/src/containers/ProjectContainer.js
@@ -1,14 +1,48 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import ProjectCarousel from '../components/ProjectCarousel';
 import ProjectList from '../components/ProjectList';
-import fetchProjects from '../actions/fetchProjects';
 import { Responsive } from 'responsive-react';
 
+import projectData from '../projects.json';
+
 class ProjectContainer extends Component {
+    constructor() {
+        super();
+        this.state = {
+            projects: [],
+            activeIndex: 0
+        }
+    }
 
     componentDidMount() {
-        this.props.fetchProjects();
+        this.setState({
+            projects: projectData.projects
+        });
+    }
+
+    nextProject = () => {
+        if ( this.state.activeIndex === this.state.projects.length - 1) {
+            this.setState({
+                activeIndex: 0
+            });
+            
+        } else {
+            this.setState(prevState => ({
+                activeIndex: prevState.activeIndex + 1
+            }));
+        }
+    }
+
+    prevProject = () => {
+        if ( this.state.activeIndex === 0 ) {
+            this.setState({
+                activeIndex: this.state.projects.length - 1
+            });
+        } else {
+            this.setState(prevState => ({
+                activeIndex: prevState.activeIndex - 1
+            }));
+        }
     }
 
     render() {
@@ -18,31 +52,19 @@ class ProjectContainer extends Component {
                 <Responsive
                     displayIn={["Laptop", "Tablet"]}>
                     <ProjectCarousel 
-                        project={this.props.projects[this.props.active]}
-                        nextProject={this.props.nextProject}
-                        prevProject={this.props.prevProject}
-                        activeIndex={this.props.active} />
+                        project={this.state.projects[this.state.activeIndex]}
+                        nextProject={this.nextProject}
+                        prevProject={this.prevProject}
+                        activeIndex={this.state.activeIndex} />
                 </Responsive>
                 <Responsive
                     displayIn={["Mobile"]}>
                     <ProjectList
-                        projects={this.props.projects} />
+                        projects={this.state.projects} />
                 </Responsive>
             </section>
         )
     }
 }
 
-const mapStateToProps = state => {
-    return { projects: state.projects.list, active: state.projects.activeIndex }
-}
-
-const mapDispatchToProps = dispatch => {
-    return {
-        fetchProjects: () => dispatch(fetchProjects()),
-        nextProject: () => dispatch({ type: "NEXT_PROJECT" }),
-        prevProject: () => dispatch({ type: "PREVIOUS_PROJECT" })
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(ProjectContainer);
+export default ProjectContainer;
