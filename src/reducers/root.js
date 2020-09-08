07145d390d8b9bb64132d5fbc3a362fbb5788345
Christian Cain <christian22cain@yahoo.com>
2020-09-08 16:04:31 -0500

    removed need for client directory

--- src/reducers/root.js
+++ src/reducers/root.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
-import projects from './projects';
 import blogs from './blogs';
 import loading from './loading';
 
-export default combineReducers({ projects, blogs, loading });
+export default combineReducers({  blogs, loading });
